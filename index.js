b8acd861391c20951992d58d3b1adde67260ff61
George <georgejoj28@gmail.com>
2023-09-25 22:42:25 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+    
+function superbowlWin(records) {
+    const winningRecord = records.find((record) => record.result === "W");
+  
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+
+
+
+
+
